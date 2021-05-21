40b84a5aed31c9435ec53e46e8bed37dcc81457c
Alexey Toakr <tokar.a.v@yandex.ru>
2021-05-21 14:22:59 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mvn -B clean dependency:copy-dependencies package || exit 1
-docker build -t hdghg/still . || exit 1
+docker build -t hdghg/still:0.2 . || exit 1
 docker push hdghg/still:0.2
